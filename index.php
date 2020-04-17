46aa2bd31e58bebf9fc42d5a9299fc5585e0faa5
Cmaz1983 <39374805+Cmaz1983@users.noreply.github.com>
2020-04-17 17:28:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -816,74 +816,7 @@ END;
         </section><!-- #testimonials -->
 
 
-        <!--==========================
-      Clients Section
-    ============================-->
-        <section id="clients" class="section-bg">
-
-            <div class="container">
-
-                <div class="section-header">
-                    <h3>Associations </h3>
-                    <p>Please find below a few of my associations. </p>
-                </div>
-
-                <div class="row no-gutters clients-wrap clearfix wow fadeInUp">
-
-                    <div class="col-lg-3 col-md-4 col-xs-6">
-                        <div class="client-logo">
-                            <img src="img/clients/client-1.png" class="img-fluid" alt="">
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 col-md-4 col-xs-6">
-                        <div class="client-logo">
-                            <img src="img/clients/client-2.png" class="img-fluid" alt="">
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 col-md-4 col-xs-6">
-                        <div class="client-logo">
-                            <img src="img/clients/client-3.png" class="img-fluid" alt="">
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 col-md-4 col-xs-6">
-                        <div class="client-logo">
-                            <img src="img/clients/client-4.png" class="img-fluid" alt="">
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 col-md-4 col-xs-6">
-                        <div class="client-logo">
-                            <img src="img/clients/client-5.png" class="img-fluid" alt="">
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 col-md-4 col-xs-6">
-                        <div class="client-logo">
-                            <img src="img/clients/client-6.png" class="img-fluid" alt="">
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 col-md-4 col-xs-6">
-                        <div class="client-logo">
-                            <img src="img/clients/client-7.png" class="img-fluid" alt="">
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 col-md-4 col-xs-6">
-                        <div class="client-logo">
-                            <img src="img/clients/client-8.png" class="img-fluid" alt="">
-                        </div>
-                    </div>
-
-                </div>
-
-            </div>
-
-        </section>
-
+       
         <!--==========================
       Contact Section
     ============================-->
